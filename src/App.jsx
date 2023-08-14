badeb8334679ac82eca344a39516cfea9240c2e5
MelaniMolina <melani.molina@epn.edu.ec>
2023-08-14 14:47:08 -0500

    UseContext

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FormularioLogin from "./paginas/FormularioLogin";
 import Dashboard from "./layout/Dashboard";
 import FundamentoUseState from "./paginas/FundamentoUseState";
 import FundamentoUseEffect from "./paginas/FundamentoUseEffect";
+import FundamentoUseContext from "./paginas/FundamentoUseContext";
+import FundamentosCustomHooks from "./paginas/FundamentosCustomHooks";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
         <Route path="/fundamentos" element={<Dashboard />}>
           <Route path="usestate" element={<FundamentoUseState />} />
           <Route path="useffect" element={<FundamentoUseEffect />} />
+          <Route path="customHooks" element={<FundamentosCustomHooks />} />
+          <Route path="usecontext" element={<FundamentoUseContext />} />
         </Route>
 
 
